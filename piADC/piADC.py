8d849ed58a615dbaaaa36f481b8f77dbf25daf52
wmax351 <wmax351@gmail.com>
2021-07-26 16:04:16 -0600

    Update piADC.py

--- piADC/piADC.py
+++ piADC/piADC.py
@@ -11,6 +11,9 @@ def main():
      i2cbus = SMBus(1)  # Create a new I2C bus
      
      # U4 I2c=0x76, U5 I2C=0x77
+     # U4 will be Pitot/Static
+     # U5 will be Pitot/AOA
+     
      
      
 
