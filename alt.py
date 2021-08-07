3489d322bbba80036a24aec811b5729d23f9b67a
wmax351 <wmax351@gmail.com>
2021-08-07 00:34:35 -0600

    working on altitude

--- alt.py
+++ alt.py
@@ -1,28 +1,10 @@
-from smbus2 import SMBus
-import adafruit_bmp3xx
+from bmp388 import BMP388
 import struct
 import time
-import board
 from math import sqrt
 from statistics import mean
 
-print(board.I2C())
-# I2C setup
-i2c = 1 #board.I2C()  # uses board.SCL and board.SDA
-bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
-
-
-bmp.pressure_oversampling = 8
-bmp.temperature_oversampling = 2
-
-while True:
-    print(
-        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
-    )
-    time.sleep(1)
-
-
-# # From plugin
+# # From fixgw plugin
 #         while True:
 #             if self.getout:
 #                 break
@@ -41,3 +23,13 @@ while True:
 #             airpress = int(self.sensor.read_pressure())
 #             self.parent.db_write(self.pkey, airpress)
 #         self.running = False
+
+
+# Create a bmp388 object to communicate with I2C.
+bmp389 = BMP388()
+
+
+while True:
+    time.sleep(0.5)
+    temperature,pressure,altitude = bmp389.get_temperature_and_pressure_and_altitude()
+    print(' Temperature = %.1f Pressure = %.2f  Altitude =%.2f '%(temperature/100.0,pressure/100.0,altitude/100.0))
